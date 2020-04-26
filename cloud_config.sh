cad0bb1bb937de27116f3765b516294eda668ff1
runoob <test@runoob.com>
2020-04-26 17:02:53 -0400

    5

--- cloud_config.sh
+++
@@ -1,2 +0,0 @@
-gcloud config set project "bookrecommendation-267223"
-gcloud config set compute/region "us-east1"
--- main.py
+++ main.py
@@ -56,4 +56,4 @@ def recommendation():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000) 
+    app.run(debug=True, port=8080) 
